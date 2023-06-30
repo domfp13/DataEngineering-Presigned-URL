3d1f9c7a6d5acca794820d58ebac9caacdba93b9
eplata <domfp13@me.com>
2023-06-30 23:21:13 +0200

    Adding dynamic name for loggin groups

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,13 +1,12 @@
 # CloudWatch Log Group for Lambda
 resource "aws_cloudwatch_log_group" "de_presigned_url_lambda_log_group" {
-  name = "/aws/lambda/de_presigned_url_lambda_log_group"
-  # retain logs for 7 days
+  name              = "/aws/lambda/${aws_lambda_function.de_presigned_url_lambda.function_name}"
   retention_in_days = 7
 }
 
 # CloudWatch Log Group for API Gateway
 resource "aws_cloudwatch_log_group" "de_presigned_url_api_gateway_log_group" {
-  name = "/aws/api-gateway/de_presigned_url_api_gateway_log_group"
+  name = "/aws/api-gateway/${aws_apigatewayv2_api.de_presigned_url_api_gateway.name}"
   # retain logs for 7 days
   retention_in_days = 7
 }
