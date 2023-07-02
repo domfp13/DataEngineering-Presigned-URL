dbf81c58d2b6ec93bd7541bd221338e7b6a7ddae
eplata <domfp13@me.com>
2023-07-02 14:13:00 +0200

    Functioning version
    This version might have some bugs but it works so far

--- getSignedURL/app.js
+++ getSignedURL/app.js
@@ -1,5 +1,14 @@
+// Created by: Enrique Plata
+
+// strict is a directive that enables strict mode in JavaScript. When strict mode is enabled, it enforces stricter rules and provides better error handling, leading to more robust and predictable code.
+'use strict'
+
 const AWS = require('aws-sdk');
+AWS.config.update({region: process.env.AWS_REGION});
 const csv = require('csv-parser');
+
+// URL expiration time in seconds, this time is for how long the presigned URL will be valid. 
+const URL_EXPIRATION_SECONDS = 300;
 
 /**
  *
@@ -16,6 +25,14 @@ const getEventFileName = function (event) {
         return event.headers['file-name'];
     }
 };
+
+// get the getApiGateWayPath "path" from the event
+const getApiGateWayPath = function (event) {
+    if (event.headers && event.headers !== "") {
+        console.log('headers: ', event.headers);
+        return event.path;
+    }
+}
 
 // Function to download the CSV file from S3
 async function downloadFileFromS3(bucketName, key) {
@@ -44,6 +61,40 @@ async function parseCSV(s3Stream) {
     });
 }
 
+/**
+ *
+ * fileName str:
+ * @param fileName string - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} - HTTP response
+ *
+ */
+const getUploadURL = async function (fileName, path, bucketName) {
+    // instanciate S3 client
+    const s3 = new AWS.S3()
+
+    const Key = path + `${fileName}`;
+
+    // Get signed URL from S3
+    const s3Params = {
+        Bucket: bucketName,
+        Key,
+        Expires: URL_EXPIRATION_SECONDS,
+        ContentType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    };
+
+    // This will keep the longs in CloudWatch
+    console.log('Params: ', s3Params);
+
+    const uploadURL = await s3.getSignedUrlPromise('putObject', s3Params);
+
+    return JSON.stringify({
+        uploadURL: uploadURL,
+        Key
+    });
+}
+
 exports.lambdaHandler = async (event, context, callback) => {
     try {
 
@@ -62,21 +113,32 @@ exports.lambdaHandler = async (event, context, callback) => {
         // Get the file name from the event
         const { BUCKET_NAME, SNS_TOPIC_ARN } = process.env;
         const bucketName = BUCKET_NAME;
+        
+        // Inside of the S3 bucket should be a file called mapping.csv which is used for validation of the files that are allowed to be uploaded
         const key = 'mapping.csv';
-        //const results = [];
 
         // Download the CSV file from S3
         const s3Stream = await downloadFileFromS3(bucketName, key);
 
         // Parse the CSV file and get the list of records
         const records = await parseCSV(s3Stream);
-        //const records = await parseCSV(s3Stream, results);
 
         // Getting the file name from API Gateway event
         let fileName = getEventFileName(event);
 
+        // Check if the file name provided in the json body is valid in the mapping.csv file
         if (records.includes(fileName)) {
-            console.log("File name is valid");
+            console.log('INFO:', "File name is valid: ", fileName);
+            
+            // Getitng the AWS API Gateway stage (prod or qc)
+            if(getApiGateWayPath(event).includes('prod')){
+                console.log('INFO:', "PROD prefix");
+                return await getUploadURL(fileName, 'prod/', bucketName);
+            }else{
+                console.log('INFO:', "QC prefix");
+                return await getUploadURL(fileName, 'qc/', bucketName);
+            }
+
         } else {
             console.log("File name is invalid");
         }
