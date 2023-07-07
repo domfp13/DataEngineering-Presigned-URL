b2de84a759cffec6aeb53f34a39e278ff7c5deb9
eplata <domfp13@me.com>
2023-07-07 09:42:46 +0200

    Improve Lambda function

--- getSignedURL/app.js
+++ getSignedURL/app.js
@@ -1,32 +1,100 @@
-// Created by: Enrique Plata
-
-// strict is a directive that enables strict mode in JavaScript. When strict mode is enabled, it enforces stricter rules and provides better error handling, leading to more robust and predictable code.
-'use strict'
+'use strict';
 
 const AWS = require('aws-sdk');
-AWS.config.update({region: process.env.AWS_REGION});
 const csv = require('csv-parser');
+const stream = require('stream');
 
-// URL expiration time in seconds, this time is for how long the presigned URL will be valid. 
+AWS.config.update({ region: process.env.AWS_REGION });
+
+// URL expiration time in seconds, this time is for how long the presigned URL will be valid.
 const URL_EXPIRATION_SECONDS = 300;
 
+// Create a single S3 client object and reuse it throughout the Lambda function
+const s3 = new AWS.S3();
+
 /**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns string fileName - File's name to be inserted into S3 bucket
- *
+ * Downloads a file from an S3 bucket and returns a readable stream.
+ * @param {string} bucketName - The name of the S3 bucket.
+ * @param {string} key - The key of the file in the S3 bucket.
+ * @returns {ReadableStream} - A readable stream of the file contents.
  */
-const getEventFileName = function (event) {
-    if (event.headers && event.headers !== "") {
-        console.log('headers: ', event.headers);
-        return event.headers['file-name'];
+const downloadFileFromS3 = async (bucketName, key) => {
+    const s3Params = { Bucket: bucketName, Key: key };
+    try {
+        const s3Object = await s3.getObject(s3Params).promise();
+        // Create a readable stream from the S3 object buffer using the stream.Readable.from() method
+        const s3Stream = stream.Readable.from(s3Object.Body);
+        return s3Stream;
+    } catch (error) {
+        console.error(`Failed to download S3 object: ${error}`);
+        throw new Error('Failed to download S3 object');
     }
 };
 
-// get the getApiGateWayPath "path" from the event
+/**
+ * Parses a CSV file from a readable stream and returns the list of records.
+ * @param {ReadableStream} s3Stream - A readable stream of the CSV file contents.
+ * @returns {Promise<Array>} - A promise that resolves to an array of records from the CSV file.
+ */
+const parseCSV = async (s3Stream) => {
+    const results = [];
+    return new Promise((resolve, reject) => {
+        s3Stream
+            .pipe(csv({
+                skipLines: (line) => line.trim() === ''
+            }))
+            .on('data', (data) => {
+                const values = Object.values(data);
+                results.push(...values);
+            })
+            .on('end', () => {
+                resolve(results);
+            })
+            .on('error', (error) => {
+                console.error(`Failed to parse CSV file: ${error}`);
+                reject(new Error('Failed to parse CSV file'));
+            });
+    });
+};
+
+/**
+ * Generates a presigned URL for uploading a file to an S3 bucket.
+ * @param {string} fileName - The name of the file to be uploaded.
+ * @param {string} path - The path prefix to be used in the S3 key.
+ * @param {string} bucketName - The name of the S3 bucket.
+ * @returns {Promise<Object>} - A promise that resolves to an HTTP response object containing the presigned URL and S3 key.
+ */
+const getUploadURL = async (fileName, path, bucketName) => {
+    const Key = `${path}${fileName}`;
+    const ContentType = fileName.endsWith('.xlsx')
+        ? 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        : 'text/csv';
+    const s3Params = {
+        Bucket: bucketName,
+        Key,
+        Expires: URL_EXPIRATION_SECONDS,
+        ContentType,
+    };
+    try {
+        const uploadURL = await s3.getSignedUrlPromise('putObject', s3Params);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                uploadURL,
+                Key,
+            }),
+        };
+    } catch (error) {
+        console.error(`Failed to generate presigned URL: ${error}`);
+        throw new Error('Failed to generate presigned URL');
+    }
+};
+
+/**
+ * Get's the path from the API Gateway event and returns astring with the path prefix.
+ * @param {string} event - JSON object containing the API Gateway event.
+ * @returns {string} - A string with the path prefix.
+ */
 const getApiGateWayPath = function (event) {
     if (event.headers && event.headers !== "") {
         console.log('headers: ', event.headers);
@@ -34,144 +102,51 @@ const getApiGateWayPath = function (event) {
     }
 }
 
-// Function to download the CSV file from S3
-async function downloadFileFromS3(bucketName, key) {
-    const s3 = new AWS.S3();
-    const s3Params = { Bucket: bucketName, Key: key };
-    const s3Stream = s3.getObject(s3Params).createReadStream();
-    return s3Stream;
-}
-
-// Function to parse the CSV file and return the list of records
-async function parseCSV(s3Stream) {
-    return new Promise((resolve, reject) => {
-        const results = [];
-        s3Stream
-            .pipe(csv({ skipLines: 1 })) // Skip the first row (header)
-            .on('data', (data) => {
-                const values = Object.values(data);
-                results.push(...values);
-            })
-            .on('end', () => {
-                resolve(results); // Return the list of values from the CSV file
-            })
-            .on('error', (error) => {
-                reject(error);
-            });
-    });
-}
-
 /**
- *
- * fileName str:
- * @param fileName string - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} - HTTP response
- *
+ * Lambda function handler that generates a presigned URL for uploading a file to an S3 bucket.
+ * @param {Object} event - The API Gateway Lambda Proxy Input Format event.
+ * @returns {Promise<Object>} - A promise that resolves to an HTTP response object containing the presigned URL and S3 key.
  */
-const getUploadURL = async function (fileName, path, bucketName) {
-    // instanciate S3 client
-    const s3 = new AWS.S3()
-
-    const Key = path + `${fileName}`;
-
-    // Determine content type based on file extension
-    let ContentType;
-    if (fileName.endsWith('.xlsx')) {
-        ContentType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-    } else if (fileName.endsWith('.csv')) {
-        ContentType = 'text/csv';
-    }
-
-    // Get signed URL from S3
-    const s3Params = {
-        Bucket: bucketName,
-        Key,
-        Expires: URL_EXPIRATION_SECONDS,
-        ContentType,
-    };
-
-    const uploadURL = await s3.getSignedUrlPromise('putObject', s3Params);
-
-    console.log('INFO:', uploadURL);
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-          uploadURL: uploadURL,
-          Key: Key
-        })
-    };    
-    
-    // return JSON.stringify({
-    //     uploadURL: uploadURL,
-    //     Key
-    // });
-}
-
-exports.lambdaHandler = async (event, context, callback) => {
+exports.lambdaHandler = async (event) => {
     try {
-
-        // Check if "file-name" header is present in the event
-        if (!event.headers || !event.headers['file-name']) {
+        const bucketName = process.env.BUCKET_NAME;
+        const key = 'mapping.csv';
+        // Download the CSV file from S3
+        const s3Stream = await downloadFileFromS3(bucketName, key);
+        // Parse the CSV file and get the list of records
+        const records = await parseCSV(s3Stream);
+        // Get the file name from the "file-name" header of the API Gateway event
+        const fileName = event.headers?.['file-name'];
+        if (!fileName) {
             const errorMessage = 'Missing "file-name" header';
-            console.error('Error:', errorMessage);
-
+            console.error(`Error: ${errorMessage}`);
             // Return an HTTP error response
             return {
                 statusCode: 400,
                 body: JSON.stringify({ error: errorMessage }),
             };
         }
-
-        // Get the file name from the event
-        const { BUCKET_NAME } = process.env;
-        const bucketName = BUCKET_NAME;
-        
-        // Inside of the S3 bucket should be a file called mapping.csv which is used for validation of the files that are allowed to be uploaded
-        const key = 'mapping.csv';
-
-        // Download the CSV file from S3
-        const s3Stream = await downloadFileFromS3(bucketName, key);
-
-        // Parse the CSV file and get the list of records
-        const records = await parseCSV(s3Stream);
-
-        // Getting the file name from API Gateway event
-        let fileName = getEventFileName(event);
-
         // Check if the file name provided in the json body is valid in the mapping.csv file
-        if (records.includes(fileName)) {
-            console.log('INFO:', "File name is valid: ", fileName);
-            
-            // Getitng the AWS API Gateway stage (prod or qc)
-            if(getApiGateWayPath(event).includes('prod')){
-                console.log('INFO:', "PROD prefix");
-                return await getUploadURL(fileName, 'prod/', bucketName);
-            }else{
-                console.log('INFO:', "QC prefix");
-                return await getUploadURL(fileName, 'qc/', bucketName);
-            }
-
+        if (await records.includes(fileName)) {
+            // Determine the path prefix based on the API Gateway stage (prod or qc)
+            const path = getApiGateWayPath(event).includes('prod') ? 'prod/' : 'qc/';
+            // Generate a presigned URL for uploading the file to S3
+            return await getUploadURL(fileName, path, bucketName);
         } else {
-            console.log("File name is invalid");
-
             const errorMessage = 'Unsupported file, is not part of the mapping.csv list';
-            console.error('Error:', errorMessage);
-
+            console.error(`Error: ${errorMessage}`);
             // Return an HTTP error response
             return {
                 statusCode: 415,
                 body: JSON.stringify({ error: errorMessage }),
             };
         }
-        
     } catch (error) {
-        console.error('Error:', error);
+        console.error(`Error: ${error}`);
+        // Return an HTTP error response
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: error }),
+            body: JSON.stringify({ error: error.message }),
         };
     }
-}
+};
